fa3cc1e9ac1af359da0632343d1400a70f5f11eb
Nikhil <nikhildeshmukh76@gmail.com>
2026-08-31 19:24:08 +0400

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        if (prices is null || prices.Length < 1) return 0;
+
+        int minPrice = int.MaxValue;
+        int maxProfit = 0;
+        for (int i=0; i<prices.Length; i++)
+        {
+            if (prices[i] < minPrice)
+                minPrice = prices[i];
+
+            int profit = prices[i] - minPrice;
+            maxProfit = Math.Max(maxProfit, profit);
+        }
+        return maxProfit;
+    }
+}
