b645cdcd712457dbe10779bcc51873d054dacb08
Nikhil <nikhildeshmukh76@gmail.com>
2026-09-11 13:21:51 +0400

    Add: sum-of-two-integers - submission-0

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-0.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int GetSum(int a, int b) {
+        while (b != 0)
+        {
+            int carry = (a & b) << 1; // carry
+            a = a ^ b;                // sum without carry
+            b = carry;                // add carry in next iteration
+        }
+        return a;
+    }
+}
