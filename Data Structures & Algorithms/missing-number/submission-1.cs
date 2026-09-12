ef670038b2640b143d3e0b2274a3c68738a9bc71
Nikhil <nikhildeshmukh76@gmail.com>
2026-09-12 21:53:17 +0400

    Add: missing-number - submission-1

---
+++ Data Structures & Algorithms/missing-number/submission-1.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int MissingNumber(int[] nums) {
+        int n = nums.Length;
+        int expectedSum = n * (n+1) /2;
+        int actualSum = 0;
+        foreach(int num in nums)
+        {
+            actualSum += num;
+        }
+        int missingNum = expectedSum - actualSum;
+        return missingNum;
+    }
+}
