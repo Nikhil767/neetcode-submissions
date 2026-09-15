08d2ae9065b04580e35fcd88a4d66fdef1bd0f7d
Nikhil <nikhildeshmukh76@gmail.com>
2026-09-15 17:20:44 +0400

    Add: non-cyclical-number - submission-4

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-4.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public bool IsHappy(int n) {
+        HashSet<int> seen = new();        
+        while (n != 1 && !seen.Contains(n))
+        {
+            seen.Add(n);
+            n = GetSum(n);
+        }
+        return n == 1;
+    }
+
+    private int GetSum(int n)
+    {
+        int sum=0;
+        while (n > 0)
+        {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
+}
