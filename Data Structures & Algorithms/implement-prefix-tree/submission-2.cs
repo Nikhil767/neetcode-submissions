e9be6aea8190c125be1c467324122cdcda22dba7
Nikhil <nikhildeshmukh76@gmail.com>
2026-09-24 20:30:08 +0400

    Add: implement-prefix-tree - submission-2

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-2.cs
@@ -0,0 +1,58 @@
+public class PrefixTree {
+    private class Node
+    {
+        public Node[] children = new Node[26];
+        public bool isEnd;
+    }
+    private readonly Node root;
+
+    public PrefixTree() 
+    {
+        root = new Node();
+    }
+    
+    public void Insert(string word) {
+        Node current = root;
+
+        foreach (char c in word)
+        {
+            int index = c - 'a';
+            if (current.children[index] == null)
+                current.children[index] = new Node();
+
+            current = current.children[index];
+        }
+
+        current.isEnd = true;
+    }
+    
+    public bool Search(string word) {
+        Node current = root;
+
+        foreach (char c in word)
+        {
+            int index = c - 'a';
+            if (current.children[index] == null)
+                return false;
+
+            current = current.children[index];
+        }
+
+        return current.isEnd;
+    }
+    
+    public bool StartsWith(string prefix) {
+        Node current = root;
+
+        foreach (char c in prefix)
+        {
+            int index = c - 'a';
+            if (current.children[index] == null)
+                return false;
+
+            current = current.children[index];
+        }
+
+        return true;
+    }
+}
